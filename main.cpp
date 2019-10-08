84b0951c7ed2086367aaae996ff5a610d0108525
Christian Franchin <christian@corp.kionux.com.br>
2019-10-08 11:38:20 -0300

    Parameters handling with boost program_options lib
    Signed-off-by: Christian Franchin <christian@corp.kionux.com.br>

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
-#include <ctime>
 #include <gmpxx.h>
+#include <boost/program_options.hpp>
+#include <ctime>
+#include <iostream>
+#include "Converter.hpp"
 #include "NQueensTable.hpp"
 #include "PossibilitiesGenerator.hpp"
-#include "Converter.hpp"
 
 using namespace std;
+namespace po = boost::program_options;
 
 /*
 Requirements:
@@ -13,7 +15,7 @@ Requirements:
     libboost-program-options-dev
 
 How to compile:
-    c++ *.cpp -o main -lgmpxx -lgmp && ./main
+    c++ *.cpp -o main -lgmpxx -lgmp -lboost_program_options && ./main
 
 How it should work:
     - Count available machines on the clusters
@@ -31,11 +33,36 @@ How it should work:
 
 */
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[]) {
+    //Parameter handling
+    po::options_description desc_allowed("Allowed options");
+    desc_allowed.add_options()
+        ("help", "Produce help message");
+
+    po::options_description desc_required("Required options");
+    desc_required.add_options()
+        ("table", po::value<unsigned long>()->default_value(8), "Size of the N-Queens Table");
+
+    po::options_description cmdline_options;
+    cmdline_options.add(desc_allowed).add(desc_required);
+
+    po::variables_map vm;
+    store(po::command_line_parser(argc, argv).options(cmdline_options).run(), vm);
+    notify(vm);
+
+    if(vm.count("help")){
+        cout << desc_allowed << endl;
+        cout << desc_required << endl;
+
+        return 1;
+    }
+    if(vm.count("table")){
+        cout << "Defined table size as: " << vm["table"].as<unsigned long>() << endl;
+    }
+
     // NQueensTable *stance = new NQueensTable(8);
 
     // cout << stance->serialize() << endl;
-    
     // stance->update_from_serial("5|4|7|6|0|1|2|3");
 
     // cout << stance->serialize() << endl;
@@ -43,13 +70,10 @@ int main(int argc, char const *argv[]){
 
     // delete stance;
 
-
     clock_t begin = clock();
-
 
     // vector<unsigned long *> poss = PossibilitiesGenerator::generate_all_possibilites(8, 8);
     clock_t end = clock();
-
 
     // for(auto vi : poss){
     //     for (auto vj : vi){
