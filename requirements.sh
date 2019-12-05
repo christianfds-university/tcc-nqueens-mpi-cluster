da1eb43c507d3f27e52f0b96ec91f60c709a7dbb
Christian Franchin <christianfranchin@gmail.com>
2019-12-05 17:04:53 -0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-apt-get install libgmp3-dev
+wget http://us-east-1.ec2.archive.ubuntu.com/ubuntu/pool/main/g/gmp/libgmpxx4ldbl_5.0.2+dfsg-2ubuntu1_amd64.deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/pool/main/g/gmp/libgmp10_5.0.2+dfsg-2ubuntu1_amd64.deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/pool/main/g/gmp/libgmp-dev_5.0.2+dfsg-2ubuntu1_amd64.deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/pool/main/g/gmp/libgmp3-dev_5.0.2+dfsg-2ubuntu1_amd64.deb
+dpkg -i libgmpxx4ldbl_5.0.2+dfsg-2ubuntu1_amd64.deb libgmp10_5.0.2+dfsg-2ubuntu1_amd64.deb libgmp-dev_5.0.2+dfsg-2ubuntu1_amd64.deb libgmp3-dev_5.0.2+dfsg-2ubuntu1_amd64.deb
+
+rm libgmpxx4ldbl_5.0.2+dfsg-2ubuntu1_amd64.deb libgmp10_5.0.2+dfsg-2ubuntu1_amd64.deb libgmp-dev_5.0.2+dfsg-2ubuntu1_amd64.deb libgmp3-dev_5.0.2+dfsg-2ubuntu1_amd64.deb
